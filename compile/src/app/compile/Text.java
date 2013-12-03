94df8fd7db0f5dbe6980b7b3f92206cb88c88bd7
hamsterCrumbs <hamsterCrumbs@google.com>
2013-12-03 23:46:54 +0800

    Text Class
    clear thingy

--- compile/src/app/compile/Text.java
+++ compile/src/app/compile/Text.java
@@ -25,33 +25,27 @@ import java.io.*;
 public class Text extends JFrame implements ActionListener {
 	private TextArea textArea = new TextArea();
 	private TextArea console = new TextArea();
-	private MenuBar menuBar = new MenuBar(); // first, create a MenuBar item
-	private Menu file = new Menu(); // our File menu
-	// what's going in File? let's see...
-	private MenuItem openFile = new MenuItem();  // an open option
-	private MenuItem saveFile = new MenuItem(); // a save option
+	private MenuBar menuBar = new MenuBar();
+	private Menu file = new Menu();
+	private MenuItem openFile = new MenuItem();
+	private MenuItem saveFile = new MenuItem();
 	private MenuItem compile = new MenuItem();
-	private MenuItem close = new MenuItem(); // and a close option!
+	private MenuItem close = new MenuItem();
 	
 	public Text() {
 		redirectSystemStreams();
 		
-		this.setSize(500, 500); // set the initial size of the window
-		this.setTitle("Just Another Compiler"); // set the title of the window
-		setDefaultCloseOperation(EXIT_ON_CLOSE); // set the default close operation (exit when it gets closed)
-		this.textArea.setFont(new Font("Century Gothic", Font.BOLD, 12)); // set a default font for the TextArea
-		
-		this.getContentPane().setLayout(new BorderLayout()); // the BorderLayout bit makes it fill it automatically
+		this.setSize(500, 500);
+		this.setTitle("Just Another Compiler");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.textArea.setFont(new Font("Consolas", Font.PLAIN, 12));
+		this.getContentPane().setLayout(new BorderLayout());
 		this.getContentPane().add(textArea);
 		this.getContentPane().add(console, BorderLayout.PAGE_END);
 		this.console.setEditable(false);
 		
                 this.textArea.addKeyListener
               (new KeyAdapter() {
-                 /*public void keyPressed(KeyEvent e) {
-                      //CALL SYNTAXCHECKER HERE
-                        System.out.println(textArea.getText());
-                   }*/
                  public void keyReleased(KeyEvent e) {
 
                      
@@ -59,100 +53,70 @@ public class Text extends JFrame implements ActionListener {
                  }
               );
 
-		// add our menu bar into the GUI
 		this.setMenuBar(this.menuBar);
-		this.menuBar.add(this.file); // we'll configure this later
+		this.menuBar.add(this.file);
 		
-		// first off, the design of the menuBar itself. Pretty simple, all we need to do
-		// is add a couple of menus, which will be populated later on
 		this.file.setLabel("File");
+		this.openFile.setLabel("Open");
+		this.openFile.addActionListener(this);
+		this.openFile.setShortcut(new MenuShortcut(KeyEvent.VK_O, false));
+		this.file.add(this.openFile);
 		
-		// now it's time to work with the menu. I'm only going to add a basic File menu
-		// but you could add more!
-		
-		// now we can start working on the content of the menu~ this gets a little repetitive,
-		// so please bare with me!
-		
-		// time for the repetitive stuff. let's add the "Open" option
-		this.openFile.setLabel("Open"); // set the label of the menu item
-		this.openFile.addActionListener(this); // add an action listener (so we know when it's been clicked
-		this.openFile.setShortcut(new MenuShortcut(KeyEvent.VK_O, false)); // set a keyboard shortcut
-		this.file.add(this.openFile); // add it to the "File" menu
-		
-		// and the save...
 		this.saveFile.setLabel("Save");
 		this.saveFile.addActionListener(this);
 		this.saveFile.setShortcut(new MenuShortcut(KeyEvent.VK_S, false));
 		this.file.add(this.saveFile);
 		
-		// compile button
 		this.compile.setLabel("Compile");
 		this.compile.addActionListener(this);
 		this.compile.setShortcut(new MenuShortcut(KeyEvent.VK_F5, false));
 		this.file.add(this.compile);
 		
-		// and finally, the close option
+		
 		this.close.setLabel("Close");
-		// along with our "CTRL+F4" shortcut to close the window, we also have
-		// the default closer, as stated at the beginning of this tutorial.
-		// this means that we actually have TWO shortcuts to close:
-		// 1) the default close operation (example, Alt+F4 on Windows)
-		// 2) CTRL+F4, which we are about to define now: (this one will appear in the label)
 		this.close.setShortcut(new MenuShortcut(KeyEvent.VK_F4, false));
 		this.close.addActionListener(this);
 		this.file.add(this.close);
 	}
 	
 	public void actionPerformed (ActionEvent e) {
-		// if the source of the event was our "close" option
 		if (e.getSource() == this.close)
-			this.dispose(); // dispose all resources and close the application
+			this.dispose();
 		
-		//compile option
 		if (e.getSource() == this.compile) {
+			console.setText("");
 			try {
 				process(textArea.getText());
 			} catch (IOException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 		}
 		
-		// if the source was the "open" option
 		else if (e.getSource() == this.openFile) {
-			JFileChooser open = new JFileChooser(); // open up a file chooser (a dialog for the user to browse files to open)
-			int option = open.showOpenDialog(this); // get the option that the user selected (approve or cancel)
-			// NOTE: because we are OPENing a file, we call showOpenDialog~
-			// if the user clicked OK, we have "APPROVE_OPTION"
-			// so we want to open the file
+			JFileChooser open = new JFileChooser();
+			int option = open.showOpenDialog(this);
+			
 			if (option == JFileChooser.APPROVE_OPTION) {
-				this.textArea.setText(""); // clear the TextArea before applying the file contents
+				this.textArea.setText("");
 				try {
-					// create a scanner to read the file (getSelectedFile().getPath() will get the path to the file)
 					Scanner scan = new Scanner(new FileReader(open.getSelectedFile().getPath()));
-					while (scan.hasNext()) // while there's still something to read
-						this.textArea.append(scan.nextLine() + "\n"); // append the line to the TextArea
-				} catch (Exception ex) { // catch any exceptions, and...
-					// ...write to the debug console
+					while (scan.hasNext())
+						this.textArea.append(scan.nextLine() + "\n");
+				} catch (Exception ex) {
 					System.out.println(ex.getMessage());
 				}
 			}
 		}
 		
-		// and lastly, if the source of the event was the "save" option
 		else if (e.getSource() == this.saveFile) {
-			JFileChooser save = new JFileChooser(); // again, open a file chooser
-			int option = save.showSaveDialog(this); // similar to the open file, only this time we call
-			// showSaveDialog instead of showOpenDialog
-			// if the user clicked OK (and not cancel)
+			JFileChooser save = new JFileChooser();
+			int option = save.showSaveDialog(this);
 			if (option == JFileChooser.APPROVE_OPTION) {
 				try {
-					// create a buffered writer to write to a file
 					BufferedWriter out = new BufferedWriter(new FileWriter(save.getSelectedFile().getPath()));
-					out.write(this.textArea.getText()); // write the contents of the TextArea to the file
-					out.close(); // close the file stream
-				} catch (Exception ex) { // again, catch any exceptions and...
-					// ...write to the debug console
+					out.write(this.textArea.getText());
+					out.close();
+				} catch (Exception ex) {
 					System.out.println(ex.getMessage());
 				}
 			}
@@ -185,7 +149,6 @@ public class Text extends JFrame implements ActionListener {
         System.out.println(cmp.functionList.size());
     }
 	
-	//The following codes set where the text get redirected. In this case, jTextArea1    
 	  private void updateTextArea(final String text) {
 	    SwingUtilities.invokeLater(new Runnable() {
 	      public void run() {
@@ -194,7 +157,6 @@ public class Text extends JFrame implements ActionListener {
 	    });
 	  }
 	 
-	//Followings are The Methods that do the Redirect, you can simply Ignore them. 
 	  private void redirectSystemStreams() {
 	    OutputStream out = new OutputStream() {
 	      @Override
